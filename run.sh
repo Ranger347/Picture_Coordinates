1860a74758b79af5724220149d43e984602d87a3
Ranger347 <mbucy347@gmail.com>
2024-10-15 15:18:16 -0400

    added documentation to the run script

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@
 This script uses exiftool to find the GPS Position of the provided file. 
 If there is no file the program ends.
 The outputs of the file are sent to a python script to calculate the coordinates as a decimal.
+
+How to use:
+./run.sh /Volumes/Drone/DCIM/100Media/{Filename}.MP4
+
+The program will output coordinates to stdout and copy them to the MacOS clipboard. Apple has a universal clipboard between devices so just go to the phone and paste the coordinates into the adjust coordinates section of the video. :)
 '
 
 get_gps_coords() {
